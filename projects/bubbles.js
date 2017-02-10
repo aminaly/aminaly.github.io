48680740b4c8dd39a4bf8c51457eaa7053c9daec
aminaly <ly.ami@husky.neu.edu>
2017-02-10 00:25:28 -0500

    updating bubble data

--- projects/bubbles.js
+++ projects/bubbles.js
@@ -1,85 +1,427 @@
-var svg = d3.select("svg"),
-	width = +svg.attr("width");
+function bubbleChart() {
+	// Constants for sizing
+	var width = 940;
+	var height = 600;
 
-var format = d3.format(",d");
+	// tooltip for mouseover functionality
+	var tooltip = floatingTooltip('gates_tooltip', 240);
 
-var color = d3.scaleOrdinal(d3.schemeCategory20c);
+	// Locations to move bubbles towards, depending
+	// on which view mode is selected.
+	var center = {
+		x: width / 2,
+		y: height / 2
+	};
 
-var pack = d3.pack()
-	.size([width, width])
-	.padding(1.5);
+	var incomeCenters = {
+		low: {
+			x: width / 4,
+			y: height / 2
+		},
+		lowmid: {
+			x: width / 3,
+			y: height / 2
+		},
+		highmid: {
+			x: 2 * width / 4,
+			y: height / 2
+		},
+		high: {
+			x: 2 * width / 3,
+			y: height / 2
+		}
+	};
 
-d3.csv("", function (d) {
-	d.value = +d.value;
-	if (d.value) return d;
-}, function (error, classes) {
-	if (error) throw error;
+	// X locations of the year titles.
+	var yearsTitleX = {
+		2008: 160,
+		2009: width / 2,
+		2010: width - 160
+	};
 
-	var root = d3.hierarchy({
-			children: classes
-		})
-		.sum(function (d) {
-			return d.value;
-		})
-		.each(function (d) {
-			if (id = d.data.id) {
-				var id, i = id.lastIndexOf(".");
-				d.id = id;
-				d.package = id.slice(0, i);
-				d.class = id.slice(i + 1);
-			}
+	// @v4 strength to apply to the position forces
+	var forceStrength = 0.03;
+
+	// These will be set in create_nodes and create_vis
+	var svg = null;
+	var bubbles = null;
+	var nodes = [];
+
+	// Charge function that is called for each node.
+	// As part of the ManyBody force.
+	// This is what creates the repulsion between nodes.
+	//
+	// Charge is proportional to the diameter of the
+	// circle (which is stored in the radius attribute
+	// of the circle's associated data.
+	//
+	// This is done to allow for accurate collision
+	// detection with nodes of different sizes.
+	//
+	// Charge is negative because we want nodes to repel.
+	// @v4 Before the charge was a stand-alone attribute
+	//  of the force layout. Now we can use it as a separate force!
+	function charge(d) {
+		return -Math.pow(d.radius, 2.0) * forceStrength;
+	}
+
+	// Here we create a force layout and
+	// @v4 We create a force simulation now and
+	//  add forces to it.
+	var simulation = d3.forceSimulation()
+		.velocityDecay(0.2)
+		.force('x', d3.forceX()
+			.strength(forceStrength)
+			.x(center.x))
+		.force('y', d3.forceY()
+			.strength(forceStrength)
+			.y(center.y))
+		.force('charge', d3.forceManyBody()
+			.strength(charge))
+		.on('tick', ticked);
+
+	// @v4 Force starts up automatically,
+	//  which we don't want as there aren't any nodes yet.
+	simulation.stop();
+
+	// Nice looking colors - no reason to buck the trend
+	// @v4 scales now have a flattened naming scheme
+	var fillColor = d3.scaleOrdinal()
+		.domain(['low', 'medium', 'high'])
+		.range(['#d84b2a', '#beccae', '#7aa25c']);
+
+
+	/*
+	 * This data manipulation function takes the raw data from
+	 * the CSV file and converts it into an array of node objects.
+	 * Each node will store data and visualization values to visualize
+	 * a bubble.
+	 *
+	 * rawData is expected to be an array of data objects, read in from
+	 * one of d3's loading functions like d3.csv.
+	 *
+	 * This function returns the new node array, with a node in that
+	 * array for each element in the rawData input.
+	 */
+	function createNodes(rawData) {
+		// Use the max total_amount in the data as the max in the scale's domain
+		// note we have to ensure the total_amount is a number.
+		var maxAmount = d3.max(rawData, function (d) {
+			return +d.ffConsump;
 		});
 
-	var node = svg.selectAll(".node")
-		.data(pack(root)
-			.leaves())
-		.enter()
-		.append("g")
-		.attr("class", "node")
-		.attr("transform", function (d) {
-			return "translate(" + d.x + "," + d.y + ")";
+		// Sizes bubbles based on area.
+		// @v4: new flattened scale names.
+		var radiusScale = d3.scalePow()
+			.exponent(0.5)
+			.range([2, 85])
+			.domain([0, maxAmount]);
+
+		// Use map() to convert raw data into node data.
+		// Checkout http://learnjsdata.com/ for more on
+		// working with data.
+		var myNodes = rawData.map(function (d) {
+			return {
+				name: d.Country,
+				radius: radiusScale(+d.ffConsump),
+				value: +d.ffConsump,
+				group: d.Income,
+				x: Math.random() * 900,
+				y: Math.random() * 800
+			};
 		});
 
-	node.append("circle")
-		.attr("id", function (d) {
-			return d.id;
-		})
-		.attr("r", function (d) {
-			return d.r;
-		})
-		.style("fill", function (d) {
-			return color(d.package);
+		// sort them to prevent occlusion of smaller nodes.
+		myNodes.sort(function (a, b) {
+			return b.value - a.value;
 		});
 
-	node.append("clipPath")
-		.attr("id", function (d) {
-			return "clip-" + d.id;
-		})
-		.append("use")
-		.attr("xlink:href", function (d) {
-			return "#" + d.id;
-		});
+		return myNodes;
+	}
 
-	node.append("text")
-		.attr("clip-path", function (d) {
-			return "url(#clip-" + d.id + ")";
-		})
-		.selectAll("tspan")
-		.data(function (d) {
-			return d.class.split(/(?=[A-Z][^A-Z])/g);
-		})
-		.enter()
-		.append("tspan")
-		.attr("x", 0)
-		.attr("y", function (d, i, nodes) {
-			return 13 + (i - nodes.length / 2 - 0.5) * 10;
-		})
-		.text(function (d) {
-			return d;
-		});
+	/*
+	 * Main entry point to the bubble chart. This function is returned
+	 * by the parent closure. It prepares the rawData for visualization
+	 * and adds an svg element to the provided selector and starts the
+	 * visualization creation process.
+	 *
+	 * selector is expected to be a DOM element or CSS selector that
+	 * points to the parent element of the bubble chart. Inside this
+	 * element, the code will add the SVG continer for the visualization.
+	 *
+	 * rawData is expected to be an array of data objects as provided by
+	 * a d3 loading function like d3.csv.
+	 */
+	var chart = function chart(selector, rawData) {
+		// convert raw data into nodes data
+		nodes = createNodes(rawData);
 
-	node.append("title")
-		.text(function (d) {
-			return d.id + "\n" + format(d.value);
+		// Create a SVG element inside the provided selector
+		// with desired size.
+		svg = d3.select(selector)
+			.append('svg')
+			.attr('width', width)
+			.attr('height', height);
+
+		// Bind nodes data to what will become DOM elements to represent them.
+		bubbles = svg.selectAll('.bubble')
+			.data(nodes, function (d) {
+				return d.Country;
+			});
+
+		// Create new circle elements each with class `bubble`.
+		// There will be one circle.bubble for each object in the nodes array.
+		// Initially, their radius (r attribute) will be 0.
+		// @v4 Selections are immutable, so lets capture the
+		//  enter selection to apply our transtition to below.
+		var bubblesE = bubbles.enter()
+			.append('circle')
+			.classed('bubble', true)
+			.attr('r', 0)
+			.attr('fill', function (d) {
+				return fillColor(d.group);
+			})
+			.attr('stroke', function (d) {
+				return d3.rgb(fillColor(d.group))
+					.darker();
+			})
+			.attr('stroke-width', 2)
+			.on('mouseover', showDetail)
+			.on('mouseout', hideDetail);
+
+		// @v4 Merge the original empty selection and the enter selection
+		bubbles = bubbles.merge(bubblesE);
+
+		// Fancy transition to make bubbles appear, ending with the
+		// correct radius
+		bubbles.transition()
+			.duration(2000)
+			.attr('r', function (d) {
+				return d.radius;
+			});
+
+		// Set the simulation's nodes to our newly created nodes array.
+		// @v4 Once we set the nodes, the simulation will start running automatically!
+		simulation.nodes(nodes);
+
+		// Set initial layout to single group.
+		groupBubbles();
+	};
+
+	/*
+	 * Callback function that is called after every tick of the
+	 * force simulation.
+	 * Here we do the acutal repositioning of the SVG circles
+	 * based on the current x and y values of their bound node data.
+	 * These x and y values are modified by the force simulation.
+	 */
+	function ticked() {
+		bubbles
+			.attr('cx', function (d) {
+				return d.x;
+			})
+			.attr('cy', function (d) {
+				return d.y;
+			});
+	}
+
+	/*
+	 * Provides a x value for each node to be used with the split by income
+	 * x force.
+	 */
+	function nodeYearPos(d) {
+		return incomeCenters[d.Income].x;
+	}
+
+
+	/*
+	 * Sets visualization in "single group mode".
+	 * The year labels are hidden and the force layout
+	 * tick function is set to move all nodes to the
+	 * center of the visualization.
+	 */
+	function groupBubbles() {
+		hideYearTitles();
+
+		// @v4 Reset the 'x' force to draw the bubbles to the center.
+		simulation.force('x', d3.forceX()
+			.strength(forceStrength)
+			.x(center.x));
+
+		// @v4 We can reset the alpha value and restart the simulation
+		simulation.alpha(1)
+			.restart();
+	}
+
+
+	/*
+	 * Sets visualization in "split by year mode".
+	 * The year labels are shown and the force layout
+	 * tick function is set to move nodes to the
+	 * yearCenter of their data's year.
+	 */
+	function splitBubbles() {
+		showYearTitles();
+
+		// @v4 Reset the 'x' force to draw the bubbles to their year centers
+		simulation.force('x', d3.forceX()
+			.strength(forceStrength)
+			.x(nodeYearPos));
+
+		// @v4 We can reset the alpha value and restart the simulation
+		simulation.alpha(1)
+			.restart();
+	}
+
+	/*
+	 * Hides Year title displays.
+	 */
+	function hideYearTitles() {
+		svg.selectAll('.year')
+			.remove();
+	}
+
+	/*
+	 * Shows Year title displays.
+	 */
+	function showYearTitles() {
+		// Another way to do this would be to create
+		// the year texts once and then just hide them.
+		var yearsData = d3.keys(yearsTitleX);
+		var years = svg.selectAll('.year')
+			.data(yearsData);
+
+		years.enter()
+			.append('text')
+			.attr('class', 'year')
+			.attr('x', function (d) {
+				return yearsTitleX[d];
+			})
+			.attr('y', 40)
+			.attr('text-anchor', 'middle')
+			.text(function (d) {
+				return d;
+			});
+	}
+
+
+	/*
+	 * Function called on mouseover to display the
+	 * details of a bubble in the tooltip.
+	 */
+	function showDetail(d) {
+		// change outline to indicate hover state.
+		d3.select(this)
+			.attr('stroke', 'black');
+
+		var content = '<span class="name">Title: </span><span class="value">' +
+			d.Country +
+			'</span><br/>' +
+			'<span class="name">Amount: </span><span class="value">$' +
+			addCommas(d.ffConsump) +
+			'</span><br/>' +
+			'<span class="name">Year: </span><span class="value">' +
+			d.Income +
+			'</span>';
+
+		tooltip.showTooltip(content, d3.event);
+	}
+
+	/*
+	 * Hides tooltip
+	 */
+	function hideDetail(d) {
+		// reset outline
+		d3.select(this)
+			.attr('stroke', d3.rgb(fillColor(d.group))
+				.darker());
+
+		tooltip.hideTooltip();
+	}
+
+	/*
+	 * Externally accessible function (this is attached to the
+	 * returned chart function). Allows the visualization to toggle
+	 * between "single group" and "split by year" modes.
+	 *
+	 * displayName is expected to be a string and either 'year' or 'all'.
+	 */
+	chart.toggleDisplay = function (displayName) {
+		if (displayName === 'Income') {
+			splitBubbles();
+		} else {
+			groupBubbles();
+		}
+	};
+
+
+	// return the chart function from closure.
+	return chart;
+}
+
+/*
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
+
+var myBubbleChart = bubbleChart();
+
+/*
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
+function display(error, data) {
+	if (error) {
+		console.log(error);
+	}
+
+	myBubbleChart('#vis', data);
+}
+
+/*
+ * Sets up the layout buttons to allow for toggling between view modes.
+ */
+function setupButtons() {
+	d3.select('#toolbar')
+		.selectAll('.button')
+		.on('click', function () {
+			// Remove active class from all buttons
+			d3.selectAll('.button')
+				.classed('active', false);
+			// Find the button just clicked
+			var button = d3.select(this);
+
+			// Set it as the active button
+			button.classed('active', true);
+
+			// Get the id of the button
+			var buttonId = button.attr('id');
+
+			// Toggle the bubble chart based on
+			// the currently clicked button.
+			myBubbleChart.toggleDisplay(buttonId);
 		});
-});
+}
+
+/*
+ * Helper function to convert a number into a string
+ * and add commas to it to improve presentation.
+ */
+function addCommas(nStr) {
+	nStr += '';
+	var x = nStr.split('.');
+	var x1 = x[0];
+	var x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+
+	return x1 + x2;
+}
+
+// Load the data.
+d3.csv('https://aminaly.github.io/projects/2011_ff.csv', display);
+
+// setup the buttons.
+setupButtons();
